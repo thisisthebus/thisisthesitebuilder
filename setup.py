3d53ffe5494060ab3cc74ed2d9f6edd35c1a66fe
jMyles <justin@justinholmes.com>
2023-08-12 16:25:08 +0200

    finally.

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+   name='thisisthesitebuilder',
+   version='1.0',
+   description='Another Opinionated Static Site Builder',
+   author='jMyles',
+   author_email='justin@justinholmes.com',
+   packages=['thisisthesitebuilder'],  # would be the same as name
+#    install_requires=['wheel', 'bar', 'greek'], #external packages acting as dependencies
+)
